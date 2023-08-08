99baff3a3b6a430a292143f537eac470d72a116b
Victoria Martinez <victoria2512m@gmail.com>
2023-08-07 23:26:16 -0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitHubPrueba;
+
+/**
+ *
+ * @author EQUIPO 2020
+ */
+public class Main {
+    public static void main(String[] args) {
+    
+    }
+}
